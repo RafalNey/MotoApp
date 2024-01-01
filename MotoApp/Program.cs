ec44a13a2006367de3308570ab57cd9bad782101
Rafal <rafal.ney@interia.pl>
2024-01-01 21:51:55 +0000

    With Program.cs

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,13 +1,4 @@
-﻿// using MotoApp.Entities;
-// using MotoApp.Repositories;
-
-// var employeeRepository = new GenericRepository<Employee, int>();
-// employeeRepository.Add(new Employee { FirstName = "Adam" });
-// employeeRepository.Add(new Employee { FirstName = "Piotr" });
-// employeeRepository.Add(new Employee { FirstName = "Zuzia" });
-// employeeRepository.Save();
-
-using MotoApp;
+﻿using MotoApp;
 
 var stack = new BasicStack();
 
